33f93738113d0e3fe0c476c11f803fe295c7e9d3
Luca Chaboissier <lucachaboissier@outlook.com>
2025-03-03 13:56:11 +0100

    fix(): errors conditions added in POST/PUT methods

--- services/actor.ts
+++ services/actor.ts
@@ -8,12 +8,18 @@ export function getActorByIdService(id: number): Actor {
     return getActorById(id);
 }
 
-export function createActorService(Actor: Actor) {
-    return createActor(Actor);
+export function createActorService(actor: Actor) {
+    if (actor.firstName === null || actor.lastName === null) {
+        throw new Error("First name or last name are required.");
+    }
+    return createActor(actor);
 }
 
-export function updateActorService(id: number, Actor: Actor) {
-    return updateActor(id, Actor);
+export function updateActorService(id: number, actor: Actor) {
+    if (typeof id !== "number" || id <= 0) {
+        throw new Error("Actor ID must be a positive number.");
+    }
+    return updateActor(id, actor);
 }
 
 export function deleteActorService(id: number) {

24d94c1b9f832fcd97223a4deb7884cceb67ff4d
MaxChevalier <maxime.chevalier@ynov.com>
2025-03-04 11:26:27 +0100

    fix logger

--- middlewares/errorMiddleware.ts
+++ middlewares/errorMiddleware.ts
@@ -1,19 +1,36 @@
 // deno-lint-ignore-file
 import { Context, isHttpError } from "https://deno.land/x/oak/mod.ts";
 
-const logFilePath = "./log/error.log";
+const baseLogFilePath = "./log";
 
 /**
  * Fonction pour enregistrer les erreurs dans un fichier de log
  */
 async function logError(message: string) {
+
+    const logFilePath = `${baseLogFilePath}/error-${new Date().toISOString().split("T")[0]}.log`;
+
+    //create directory if it doesn't exist
+    try {
+        await Deno.mkdir(baseLogFilePath, { recursive: true });
+    }
+    catch (createDirError) {
+        throw new Error(`Impossible de créer le répertoire de log: ${createDirError}`);
+    }
+    // create file if it doesn't exist
+    try {
+        await Deno.writeTextFile(logFilePath, "", { create: true });
+    } catch (createError) {
+        throw new Error(`Impossible de créer le fichier de log: ${createError}`);
+    }
+
     const logMessage = `[${new Date().toISOString()}] ERROR: ${message}\n`;
     console.error(logMessage);
 
     try {
         await Deno.writeTextFile(logFilePath, logMessage, { append: true });
     } catch (writeError) {
-        console.error("Impossible d'écrire dans le fichier de log:", writeError);
+        throw new Error(`Impossible d'écrire dans le fichier de log: ${writeError}`);
     }
 }
 
